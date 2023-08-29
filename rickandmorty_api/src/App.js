5494af294aeda278635744c93a9aa9a9a3959879
Mar Bautista <mar_piscis@live.com.mx>
2023-08-28 19:53:56 -0600

    USAMOS los HOOkS de React

--- rickandmorty_api/src/App.js
+++ rickandmorty_api/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
+
+  //TENEMOS NUESTRA L+OGICA
+//LE INDICO QUE MI ESTADO ESTA VACIO Y QUE SE VA A LLEVAR /POBLAR DE LOS DATOS QUE VIEBE EN LA API
+  const [character, setResults] = useState([])
+  //fetch("https://rickandmortyapi.com/api/character")
+  //.then(response =>  response.json())
+  //.then(data => console.log(data))
+  //.catch(error => console.log(error)) 
+//INDIco la ruta o el endpoint de la api
+  const URI = "https://rickandmortyapi.com/api/character"
+  
+  const getCharacters = () => {
+    fetch()
+    //esta respuesta la tenemos que convertir  a un objeto de js
+    .then((response) => response.json())
+    .then(data => setResults(data.results))
+    .catch((error) => console.log(error)) 
+  }
+  
+  //CON DEPENDECIA VACIA, LA LLAMADA SE EEJECUTA UNA VEZ RN LO QUE SE CARGA MI COMPONENTE
+  // Y SE RENDERIZA
+  useEffect(() => {
+    getCharacters(URI) //CUANDO SE CARGUE EL COMPONENTE, LLAMA AL METODO Y EJECUTA LA FUNCION ANTERIOR.
+  }, [] )
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <>
+       <Navbar brand="Rick and Morty">
+
+       </Navbar>
+      </>
+    )
   );
 }
 
