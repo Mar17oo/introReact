0d768b6010cade0925acf1298a115f31594db7df
Mar Bautista <mar_piscis@live.com.mx>
2023-08-28 20:22:38 -0600

    Consumismo los datos de la api de nuestro componenente

--- rickandmorty_api/src/Components/Character/Character.js
+++ rickandmorty_api/src/Components/Character/Character.js
@@ -9,7 +9,16 @@ const Character = ({character}) =>{
                     (
                         <div key={index} className="col-lg-3 col-md-6 col-sm-12 mb-4">
                             <div className="card">
-                                <img className="card-img-top" src={item.image}/>
+                                <img className="card-img-top" src={item.image} alt= "character"/>
+                                <div className="card-body">
+                                    <h3 className="card-title"> {item.name}</h3>
+                                    <hr/> 
+                                    <p className="card-text">Species: {item.species}</p>   
+                                    <p className="card-text">Gender: {item.gender}</p> 
+                                    <p className="card-text">Location: {item.location.name}</p> 
+                                    <p className="card-text">Status: {item.status}</p>  
+
+                                </div>
                             </div>
                         </div>
                     ))
