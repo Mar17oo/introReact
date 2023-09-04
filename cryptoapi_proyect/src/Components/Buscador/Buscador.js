74ee8409edfa3f0d3378be069af9bc2b71ae4d0d
Mar Bautista <mar_piscis@live.com.mx>
2023-09-04 17:01:08 -0600

    CAMABIOS

--- cryptoapi_proyect/src/Components/Buscador/Buscador.js
+++ cryptoapi_proyect/src/Components/Buscador/Buscador.js
@@ -1,35 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-// El componente Buscador recibe dos propiedades:
-// - buscador: Un array de resultados de búsqueda
-// - setSearch: Una función para actualizar el término de búsqueda en App.js
 const Buscador = ({ buscador, setSearch }) => {
   return (
     <div className="container mt-4">
       <h1>CoinGecko Coin Search</h1>
       <div className="input-group mb-3">
-        {/* Input controlado por el valor de 'buscador' y la función 'setSearch' */}
         <input
           type="text"
           className="form-control"
           placeholder="Search for coins"
-          value={buscador} // Mostramos el valor de 'buscador' en el campo de entrada
-          onChange={(e) => setSearch(e.target.value)} // Cuando cambia el valor, llamamos a 'setSearch' para actualizarlo
+          value={buscador} 
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
       <ul className="list-group">
-        {/* Mapeamos los resultados y los mostramos como elementos de lista */}
-        {buscador.map((result, index) => (
-          <li key={index} className="list-group-item">
-            {result.name} ({result.symbol})
-          </li>
-        ))}
+        {Array.isArray(buscador) && buscador.length > 0 ? ( // Verifica si buscador es un array y no está vacío
+          buscador.map((result, index) => (
+            <li key={index} className="list-group-item">
+              {result.name} ({result.symbol})
+            </li>
+          ))
+        ) : (
+          <li className="list-group-item">No results found</li> // Mensaje si no hay resultados
+        )}
       </ul>
     </div>
   );
 }
 
-
-
-
-export default Buscador
+export default Buscador;
